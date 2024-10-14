d16dce5c35c9092b53126a46c35d3204cf20b127
Yashi <srivastavayashi494@gmail.com>
2024-10-14 05:40:21 +0530

    first commit

---
+++ calc-fe/src/constants.ts
@@ -0,0 +1,16 @@
+const SWATCHES=[
+    '#FF0000', // Red
+    '#00FF00', // Green
+    '#0000FF', // Blue
+    '#FFFF00', // Yellow
+
+    '#FF00FF', // Magenta
+    '#FFA500', // Orange
+    '#800080', // Purple
+    '#FFC0CB', // Pink
+    '#A52A2A', // Brown
+    '#808080', // Gray
+    '#ffffff',
+
+]
+export {SWATCHES};
